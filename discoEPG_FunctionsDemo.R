7294353c1a8fe1db3388454e76b5fbfe1bd24fd6
C <c@Cs-MacBook-Pro.local>
2023-02-03 07:20:37 -0800

    Add 'discoEPG_FunctionsDemo.R' (script w/examples to use discoEPG package).

---
+++ discoEPG_FunctionsDemo.R
@@ -0,0 +1,168 @@
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+# ----- R package "discoEPG" -- Function Demonstration Code
+# ------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
+
+
+# clear the environment of all objects (beware!)
+# rm(list = ls())
+
+# install discoEPG package
+if(!require(remotes)) install.packages('remotes')
+remotes::install_github("nalamvj/EPG_ANALYSIS/discoEPG")
+
+# load package
+require(discoEPG)
+
+# load parameter metadata
+paramMetadata <- read.csv("/Users/c/Documents/EPG Project/EPGShinyApp/Deployed/ParameterMetadata.csv")
+colnames(paramMetadata) <- tolower(colnames(paramMetadata))
+paramMetadata$acronym <- gsub('\\*\\d+$', '', paramMetadata$acronym) # remove asterices
+
+# create waveform labels
+waveform_labels = c("np" = 1,
+                    "C" = 2,
+                    "E1e" = 3,
+                    "E1" = 4,
+                    "E2" = 5,
+                    "F" = 6,
+                    "G" = 7,
+                    "pd-S" = 8,
+                    "pd-S-II-2" = 9,
+                    "pd-S-II-3" = 10,
+                    "pd-L" = 11,
+                    "pd-L-II-2" = 13,
+                    "pd-L-II-3" = 14,
+                    "end" = 12)
+
+
+# ------------------------------------------------------------------------------
+# ----- Function 1: read_ana -- Read 1 ANA file.
+# ------------------------------------------------------------------------------
+
+# call 'read_ana' function and store results in a list object
+ana_out <- read_ana(filepath = "/Users/c/Documents/EPG Project/Info for Clay/Grids/1-15-2018 day-ch7_VJN.ANA",
+                    validate = TRUE, # perform data validation
+                    waveform_labels = waveform_labels)
+
+# the list object has 2 items: 'data' and 'errors'
+# data frame with labelled waveforms
+head(ana_out$data)
+# validation errors (character; or NULL if there were no validation errors)
+ana_out$errors
+
+# ------------------------------------------------------------------------------
+# ----- Function 2: combine_ana -- Read multiple ANA files and combine.
+# ------------------------------------------------------------------------------
+
+# folder (directory) for .ANA files 
+ana_dir <- "/Users/c/Documents/EPG Project/Info for Clay/Grids/"
+
+# character vector of all .ANA files in above folder
+filenames <- list.files(path = ana_dir, pattern = ".ANA")
+
+# read all .ANA files in directory into a single dataframe
+anaDatList <- combine_ana(directory = ana_dir,
+                          filenames = filenames,
+                          validate = TRUE, # perform data validation
+                          waveform_labels = waveform_labels)
+
+# extract data
+anaData <- anaDatList$data
+
+# extract and print validation errors
+cat(anaDatList$errors)
+
+# TODO: Refactor combine_ana to give a df with errors and filenames, use this to filter data
+# subset ANA data list to include only those ANA files with no validation errors
+# datList <- datList[-errorInd]
+
+
+
+# ------------------------------------------------------------------------------
+# ----- Function 3: ana_to_parameters -- Convert all ANA data into parameters.
+# ------------------------------------------------------------------------------
+
+# pass the above data frame to this function and return computed parameter values in a data frame
+paramData <- ana_to_parameters(ana_df = anaData, waveform_labels = waveform_labels)
+
+
+# ------------------------------------------------------------------------------
+# ----- FUNCTION 4: compute_summary_stats -- Compute summary statistics, by treatment.
+# ------------------------------------------------------------------------------
+
+# NOTE: We must assign treatments to 'paramData' df before computing summary statistics.
+# (This is normally done in Shiny app via an interactive table)
+
+# initialize 'treatment' column
+paramData$treatment <- NULL
+
+# get all filenames in master data frame
+allfiles <- unique(paramData$filename)
+
+# assign treatments for files with names containing 'day' or 'night'
+dayfiles <- grep('day', allfiles, value = TRUE)
+nightfiles <- grep('night', allfiles, value = TRUE)
+paramData$treatment[paramData$filename %in% dayfiles] <- 'day'
+paramData$treatment[paramData$filename %in% nightfiles] <- 'night'
+
+# remove data from files not matching 'day' or 'night' in file name
+paramData <- subset(paramData, filename %in% c(dayfiles, nightfiles))
+
+# confirm all rows have treatment assigned
+table(paramData$treatment, useNA = 'ifany')
+
+# finally, call the function to compute summary statistics
+sumStats <- compute_summary_stats(parameter_df = paramData,
+                                  parameter_metadata = paramMetadata,
+                                  time_units='minutes',
+                                  treatments=NULL,
+                                  acronyms=NULL,
+                                  alpha = 0.05,
+                                  p_adjust_method='bonferroni')
+
+# ------------------------------------------------------------------------------
+# ----- FUNCTION 5: ana_to_timeseries
+# ------------------------------------------------------------------------------
+
+anaData$treatment <- NULL
+anaData$treatment[anaData$filename %in% dayfiles] <- 'day'
+anaData$treatment[anaData$filename %in% nightfiles] <- 'night'
+# remove data from files not matching 'day' or 'night' in file name
+anaData <- subset(anaData, filename %in% c(dayfiles, nightfiles))
+
+# confirm all rows have treatment assigned
+table(anaData$treatment, useNA = 'ifany')
+
+# subset to data in a single treament (one treament per kinetogram)
+day_data <- subset(anaData, treatment == 'day')
+
+# run timeseries function and return a list with data and plot
+ts_out <- ana_to_timeseries(ana_df = day_data, waveform_labels = waveform_labels)
+
+# extract data -- mode(s) of most common waveform(s) for each second
+ts_dat <- ts_out$data
+
+# show plot
+ts_out$plot
+# ------------------------------------------------------------------------------
+# ----- FUNCTION 6: ana_to_kinetogram_df
+# ------------------------------------------------------------------------------
+
+
+# --------- Function (In Progress) to Calculate Transitions and % Time Spent in each Activity ===========
+
+datList <- datList[1:2]
+
+trtKey <- data.frame(filename=sapply(datList, function(x) unique(x$filename)))
+trtKey$treatment <- rep_len(letters[1:2], nrow(trtKey))
+
+
+kineDat <- ana_to_kinetogram_df(anaDatList = datList, trtKey = trtKey)
+
+## ---- Function to convert data for a single treatment to kinetograph
+
+make_kinetogram(kineDat = kineDat, trt = 'a')
+make_kinetogram(kineDat = kineDat, trt = 'b')
+
